35b5c8f9b1a313d1c6136682b7d900b540fcef67
pamela salazar <you@example.com>
2021-08-11 10:36:53 +0200

    Part of the TopBar done

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import yeraldin from '../img/yeraldin.jpg'
+import './topbar.css'
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
+
+export default function Topbar() {
+
+    return (
+        <div className="topbar">
+            <div className="topbarWraper">
+                <div className="topLeft">
+                    <span className="logo"> Llamaadmin </span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBag">2</span>
+                    </div>
+
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBag">2</span>
+                    </div>
+
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src={yeraldin} className="topAvatar"  alt="hola" />
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
